5e05e4e3b766be231e2f8d51301fea151cb2fa1e
M1GHTY <shaz3rr@gmail.com>
2022-10-13 12:31:30 +0100

    new jenkins+groovy

--- script.groovy
+++ script.groovy
@@ -7,7 +7,7 @@ def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'nexus-docker-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh 'docker build -t localhost:8083/java-maven-app:3.0 .'
-        sh "echo ${PASS} | docker login -u ${USER} --password-stdin"
+        sh "echo ${PASS} | docker login -u ${USER} --password-stdin localhost:8083"
         sh 'docker push localhost:8083/java-maven-app:3.0'
     }
 } 
